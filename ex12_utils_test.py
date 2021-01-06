9a7e9e1b53f3f1cc139d53d01d554b87a1e89adc
orfranco <or.franco@mail.huji.ac.il>
2021-01-06 12:38:21 +0200

    load words dict

--- ex12_utils_test.py
+++ ex12_utils_test.py
@@ -1,7 +1,8 @@
 from ex12_utils import *
 
+
 def test_load_words_dict():
-    assert load_words_dict("load_dict_test_text.txt") == {"or": True, "o":
+    assert load_words_dict("TESTS\load_dict_test_text.txt") == {"or": True, "o":
                             True, "m m": True, "ss": True, "    ab": True}
 
 def test_is_valid_path():
