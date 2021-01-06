3273720f4a58c1fbd11b23428f84cba245bdeff2
orfranco <or.franco@mail.huji.ac.il>
2021-01-06 11:46:19 +0200

    load words dict

--- ex12_utils.py
+++ ex12_utils.py
@@ -1,5 +1,13 @@
 def load_words_dict(file_path):
-    pass
+    words_dict = dict()
+
+    with open(file_path, 'r') as words_file:
+        line = words_file.readline().replace("\n", "")
+        while line:
+            words_dict[line] = True
+            line = words_file.readline().replace("\n", "")
+
+    return words_dict
 
 
 def is_valid_path(board, path, words):
@@ -8,3 +16,5 @@ def is_valid_path(board, path, words):
 
 def find_length_n_words(n, board, words):
     pass
+
+print(load_words_dict("README"))
