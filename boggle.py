129fbde5773ca36d53568653246786145f2c9c2a
orfranco <or.franco@mail.huji.ac.il>
2021-01-06 15:48:56 +0200

    bogglelogic

--- boggle.py
+++ boggle.py
@@ -1,6 +1,31 @@
+import ex12_utils as utils
+from typing import Optional
+
+STARTING_SCORE = 0
+
 
 class BoggleLogic:
-    pass
+    def __init__(self, words_dict, board):
+        self.__words_dict = words_dict
+        self.__board = board
+        self.__curr_path = []
+        self.__found_words = set()
+        self.__score = STARTING_SCORE
+
+    def update_score(self, n):
+        self.__score += n**2
+
+    def get_score(self):
+        return self.__score
+
+    def insert_coord_to_path(self, coord):
+        self.__curr_path.append(coord)
+
+    def pop_coord_from_path(self):
+        self.__curr_path.pop()
+
+    def check_path(self) -> Optional[str]:
+        return utils.is_valid_path(self.__curr_path)
 
 
 class BoggleGui:
@@ -9,4 +34,5 @@ class BoggleGui:
 
 class BoggleController:
     pass
+
 
